5557386781a0447b8a63fd6ab01e9e2047507c14
AdityaG028393 <90874554+AdityaG028393@users.noreply.github.com>
2021-12-13 19:40:58 -0600

    Add files via upload

---
+++ js/PlayerArrow.js
@@ -0,0 +1,25 @@
+class PlayerArrow {
+  constructor(x, y, width, height, archerAngle) {
+    var options = {
+      isStatic: true,
+      density: 0.1
+    };
+    this.width = width;
+    this.height = height;
+    this.body = Bodies.rectangle(x, y, this.width, this.height, options);
+    this.image = loadImage("./assets/arrow.png");
+    this.archerAngle = archerAngle;
+    this.velocity = 0;
+    World.add(world, this.body);
+  }
+
+
+  display() {
+  if(keyIsDown(DOWN_ARROW)&& angle <-73){
+    angle+=73
+    Matter.Body.setAngle(this.body,angle);
+  
+  }
+   
+  }
+}
